0d671a7aa6b17907611d873f54cc09e6877df196
Peter <peter-lauridsen@live.dk>
2022-11-28 14:21:36 +0100

    navitems added

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,9 +4,9 @@ import './NavItem.css';
 const NavItem = () => {
     return ( 
         <nav>
-                <img src="img/hifi_logo" alt="" />
+                <span className='box_logo'><p className='box_logo p'>HIFI</p></span>
             <ul>
-                <li><Link to="/">Shop</Link></li>
+                <li><Link to="/Shop">Shop</Link></li>
                 <li><Link to="/AboutUs">About Us</Link></li>
                 <li><Link to="/ContactUs">Contact us</Link></li>
                 
