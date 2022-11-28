a8eaf328d1e0c34e9691b1e6e9d660a46b77730e
Peter <peter-lauridsen@live.dk>
2022-11-28 12:34:44 +0100

    linked some pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
 
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+
+export const router= createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<App />}>
+  <Route index element={<Home />} />
+  <Route path="/AboutUs" element={<AboutUs />} />
+  <Route path="/ContactUs" element={<ContactUs />} />
+  <Route path="*" element={<NotFound />} />
+ </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </React.StrictMode>
 );
 
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,1 +1,2 @@
 import { Link } from "react-router-dom";
+
