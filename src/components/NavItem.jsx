13daefdbc8c39215a5995223da83bb2f3c698e0a
Peter <peter-lauridsen@live.dk>
2022-11-29 14:02:56 +0100

    changes for navitems and productcards

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import './NavItem.css';
+import {AiOutlineUser, AiOutlineShoppingCart} from 'react-icons/ai'
 
 const NavItem = () => {
     return ( 
             <> 
         <nav>
-        <span className='box_logo'><p className='box_logo p'>HI<br/>FI</p></span>     
+        <span className='box_logo'><p className='box_logo p'><Link to="/index" className='hifiText'> HI<br/>FI</Link></p></span>     
             <ul>
                 <li><Link to="/Shop">Shop</Link></li>
                 <li><Link to="/AboutUs">About Us</Link></li>
                 <li><Link to="/ContactUs">Contact us</Link></li>   
             </ul>
-         <span className='searchBar' ><input  type="search" placeholder='Search...'/> </span>
+         <span className='searchBar' ><input  type="search" placeholder='Search...'/> <AiOutlineUser className='userIcon'/> <AiOutlineShoppingCart className='cartIcon'/></span>
         </nav>
 
 
