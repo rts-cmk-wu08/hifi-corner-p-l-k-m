873f7b09d7ed4cd155c250c759c04703a206d18e
MatasMot <98148619+MatasMot@users.noreply.github.com>
2022-12-01 12:12:56 +0100

    template and components done. no css

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -1,9 +1,15 @@
-
+import FooterContact from "../components/FooterContact"
+import FooterLicensing from "../components/FooterLicensing"
+import FooterPaymentCards from "../components/FooterPaymentCards"
+import FooterReturns from "../components/FooterReturns"
 
 const Footer = () => {
     return (
         <footer>
-            <p>Footer Template</p>
+            <FooterContact></FooterContact>
+            <FooterLicensing></FooterLicensing>
+            <FooterPaymentCards></FooterPaymentCards>
+            <FooterReturns></FooterReturns>
         </footer>
      );
 }
